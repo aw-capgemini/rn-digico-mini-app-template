9dc7105e27fd4ef1c6102709f0ebef409f667389
Arjay Walter Bernardino <arjay.walter@capgemini.com>
2024-03-06 14:27:04 +0800

    first commit

---
+++ template/src/screens/HomeScreen.tsx
@@ -0,0 +1,29 @@
+import {useNavigation} from '@react-navigation/native';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {MainStackNavigationProp} from '../navigation/MainNavigator';
+
+const HomeScreen = () => {
+  const navigation = useNavigation<MainStackNavigationProp>();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>MiniApp HomeScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  label: {
+    fontSize: 20,
+    fontWeight: '700',
+    marginBottom: 40,
+  },
+});
+
+export default HomeScreen;
